f4d3ec780ae20ebd06461ee2a2e0bd903bdb57e6
liuhui931205 <648802794@qq.com>
2018-04-18 12:37:44 +0800

    搭建框架

--- manage.py
+++ manage.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/ih_test'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
